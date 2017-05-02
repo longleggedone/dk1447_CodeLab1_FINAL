e180e90c5b0d41cebaed512bf39a31064752211b
longleggedone <longlegsclassic@gmail.com>
2017-05-02 02:10:55 -0400

    Cover and LOS working
    enemies lose track of player

--- Code Lab Final/Assets/scripts/Final TwinStick/EnemyScript.cs
+++ Code Lab Final/Assets/scripts/Final TwinStick/EnemyScript.cs
@@ -9,14 +9,34 @@ public class EnemyScript : MonoBehaviour {
 	public float pauseTime = 0.5f; //amount of time to wait at each waypoint
 
 	public Transform waypointHolder; //holds waypoints for path
+	public Transform player; //holds reference to player
+	public Color patrolColor; //holds reference to spotlight color
+
+
+	public LayerMask viewMask;
+	public Light spotlight; //the spotlight object for our enemy
+	public float viewDistance; //how far away we want our enemy to be able to spot things
+	private float viewAngle; //the angle we want our enemy to be able to spot things in
 
 	void Start(){
+		player = GameObject.FindGameObjectWithTag("Player").transform; //gets the transform of our player
+		patrolColor = spotlight.color;
+		viewAngle = spotlight.spotAngle; //sets the view angle equal to the spot angle of the spotlight
 		Vector3[] waypoints = new Vector3[waypointHolder.childCount]; //creates an array to hold a number of positions equal to the number of waypoints
 		for (int i = 0; i < waypoints.Length; i++){
 			waypoints[i] = waypointHolder.GetChild(i).position; //sets each waypoint in the array to equal the position of one of the holders child objects
 			waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z); //moves actual waypoints vertically to match the enemy y position, leaves visualized waypoint objects on ground as 'footpath'
 		}
 		StartCoroutine(FollowWaypointPath(waypoints)); //starts our coroutine for following our waypoint path
+	}
+
+	void Update(){
+		if(PlayerIsVisible()){
+			spotlight.color = Color.red;
+		}else{
+			spotlight.color = patrolColor;
+		}
+
 	}
 
 	IEnumerator FollowWaypointPath(Vector3[] waypoints){ //enemy waypoint movement coroutine, using an array of waypoints
@@ -38,11 +58,24 @@ public class EnemyScript : MonoBehaviour {
 		}
 	}
 
+	bool PlayerIsVisible(){ //bool for whether we can see the player or not
+		if(Vector3.Distance(transform.position, player.position) < viewDistance){ //if the player is within our view distance
+			Vector3 directionToPlayer = (player.position - transform.position).normalized; //gets the direction between us and the player
+			float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer); //gets the angle between our forward facing and the direction to the player
+			if(angleToPlayer < viewAngle/2f){ //if the player is within our view angle
+				if (!Physics.Linecast(transform.position, player.position, viewMask)){ //casts out a line between our position and the players, if it returns anything there is an obstacle in the way and we can't see the player
+					return true; //if nothing was hit, return true
+				}
+			}
+		}
+		return false; //if any of our three checks fail, return false
+	}
+
 	IEnumerator RotateToFace(Vector3 lookAtTarget){ //enemy rotation coroutine
 		Vector3 dirToLookAtTarget = (lookAtTarget - transform.position).normalized; //the direction from our position to the target we want to look at
 		float angleToLookAtTarget = 90 - Mathf.Atan2(dirToLookAtTarget.z, dirToLookAtTarget.x) * Mathf.Rad2Deg; //MATH MATH MATH but seriously, we find the angle we need to rotate to be looking at the target, in degrees
 
-		while(Mathf.DeltaAngle(transform.eulerAngles.y, angleToLookAtTarget) > 0.05f){ //while the difference between our rotation and the angle needed to look at the target is less than a very small value, i.e. we are looking at the target
+		while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, angleToLookAtTarget)) > 0.05f){ //while the difference between our rotation and the angle needed to look at the target is less than a very small value, i.e. we are looking at the target
 			float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, angleToLookAtTarget, rotateSpeed * Time.deltaTime);//moves an angle along the y axis towards our look at target over time using rotate speed
 			transform.eulerAngles = Vector3.up * angle; //sets our rotation on our local up axis to our new angle
 			yield return null;//DO NOT REMOVE
@@ -58,5 +91,8 @@ public class EnemyScript : MonoBehaviour {
 			prevPos = waypoint.position; //set the new previous waypoint to equal the current one
 		}
 		Gizmos.DrawLine(prevPos, startPos); //draw a line between the last waypoint and the first one, completing loop
+
+		Gizmos.color = Color.green;//set gizmo color to green
+		Gizmos.DrawRay(transform.position,transform.forward * viewDistance);//draw a line from our position forward out to our view distance
 	}
 }
